8496c691c6676bc86ff66a13bac3e519b29db02d
Claire Birge <cbirge@stanford.edu>
2025-09-09 12:05:47 -0400

    updated icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/sprite.png" />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Header />
